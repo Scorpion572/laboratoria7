0d381da33259a7ca0da82d14235a36fbc7d3fa19
Jakub Wencel <wencel_jakub@wp.pl>
2025-11-24 19:51:18 +0100

    Dodaj pliki projektów.

---
+++ laboratoria7/laboratoria7.cpp
@@ -0,0 +1,10 @@
+﻿// laboratoria7.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "Hello World!\n";
+}
+
